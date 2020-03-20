74dd1f43f55be8bb0d9bd73d29f1a4c5818598d1
suzitong <461617179@qq.com>
2020-03-20 19:05:58 +0800

    测试

---
+++ src/com/atguigu/jedis/JedisTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.jedis;
+
+import redis.clients.jedis.Jedis;
+
+public class JedisTest {
+
+	public static void main(String[] args) {
+		Jedis jedis =new Jedis("192.168.192.100",6379);
+		String ping = jedis.ping();
+		System.out.println(ping);
+	}
+
+}
